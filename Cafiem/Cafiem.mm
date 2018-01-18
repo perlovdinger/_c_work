1560b40a70cf989d81f7d83ba81c89eca9e2c23b
Per Lövdinger <per.lovdinger@volvo.com>
2018-01-18 19:45:25 +0100

    sdsdsdsd

--- Cafiem/Cafiem.mm
+++ Cafiem/Cafiem.mm
@@ -1,11 +1,13 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1505232619874" ID="ID_752122507" MODIFIED="1505232644736" TEXT="Cafiem">
-<node CREATED="1505232680616" ID="ID_1473822783" MODIFIED="1505234387673" POSITION="right" TEXT="Bilda aktiebolag">
+<node CREATED="1505232619874" ID="ID_752122507" MODIFIED="1516130518359" TEXT="Cafiem">
+<node CREATED="1516130519315" ID="ID_1328519571" MODIFIED="1516130574786" POSITION="right" TEXT="F&#xf6;rbered">
+<node CREATED="1505232680616" FOLDED="true" ID="ID_1473822783" MODIFIED="1516130538446" TEXT="Bilda aktiebolag">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1505232710268" ID="ID_1830238911" MODIFIED="1516130194081" TEXT="1. Besluta om start - uppr&#xe4;tta stiftelseurkund och bolagsordning ">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505232710268" ID="ID_1830238911" MODIFIED="1505232726227" TEXT="1. Besluta om start - uppr&#xe4;tta stiftelseurkund och bolagsordning ">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505232692373" ID="ID_189752650" MODIFIED="1505232743581">
+<node CREATED="1505232692373" ID="ID_189752650" MODIFIED="1516130194081">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -22,13 +24,13 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1505568585395" ID="ID_1113939488" MODIFIED="1505568588019" TEXT="SEB">
-<node CREATED="1505568589136" ID="ID_804761199" MODIFIED="1505568609972" TEXT="S&#xe4;tt in 50000 i tillf&#xe4;lligt f&#xf6;retagskonto"/>
-<node CREATED="1505568618438" ID="ID_1380929225" MODIFIED="1505568651559" TEXT="utf&#xe4;rda bankintyg"/>
+<node CREATED="1505568585395" ID="ID_1113939488" MODIFIED="1516130194082" TEXT="SEB">
+<node CREATED="1505568589136" ID="ID_804761199" MODIFIED="1516130194082" TEXT="S&#xe4;tt in 50000 i tillf&#xe4;lligt f&#xf6;retagskonto"/>
+<node CREATED="1505568618438" ID="ID_1380929225" MODIFIED="1516130194082" TEXT="utf&#xe4;rda bankintyg"/>
 </node>
-<node CREATED="1505232768520" ID="ID_1643405268" MODIFIED="1505232807790" TEXT="2. Teckna och betala aktier ">
+<node CREATED="1505232768520" ID="ID_1643405268" MODIFIED="1516130194082" TEXT="2. Teckna och betala aktier ">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505232784814" ID="ID_1333493032" MODIFIED="1505232800015">
+<node CREATED="1505232784814" ID="ID_1333493032" MODIFIED="1516130194082">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -60,9 +62,9 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1505232769632" ID="ID_930871980" MODIFIED="1505232822212" TEXT="3. Skriv under stiftelseurkunden ">
+<node CREATED="1505232769632" ID="ID_930871980" MODIFIED="1516130194084" TEXT="3. Skriv under stiftelseurkunden ">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505232823193" ID="ID_1836484148" MODIFIED="1505232837807">
+<node CREATED="1505232823193" ID="ID_1836484148" MODIFIED="1516130194085">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -79,9 +81,9 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1505232850840" ID="ID_421607714" MODIFIED="1505232853423" TEXT="4. Anm&#xe4;l och betala avgiften till Bolagsverket ">
+<node CREATED="1505232850840" ID="ID_421607714" MODIFIED="1516130194085" TEXT="4. Anm&#xe4;l och betala avgiften till Bolagsverket ">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505232866859" ID="ID_538816323" MODIFIED="1505232884609">
+<node CREATED="1505232866859" ID="ID_538816323" MODIFIED="1516130194085">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -109,9 +111,9 @@
 </html></richcontent>
 </node>
 </node>
-<node CREATED="1505232891585" ID="ID_8226371" MODIFIED="1505232928789" TEXT="5. Bolagsverket registrerar aktiebolaget ">
+<node CREATED="1505232891585" ID="ID_8226371" MODIFIED="1516130194087" TEXT="5. Bolagsverket registrerar aktiebolaget ">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505232915077" ID="ID_327785180" MODIFIED="1505232925971">
+<node CREATED="1505232915077" ID="ID_327785180" MODIFIED="1516130194087">
 <richcontent TYPE="NODE"><html>
   <head>
     
@@ -126,17 +128,159 @@
 </node>
 </node>
 </node>
-<node CREATED="1505233557673" ID="ID_1554261232" MODIFIED="1505567740826" POSITION="right" TEXT="Registrera en dom&#xe4;n och e-postadress ">
+<node CREATED="1505233557673" ID="ID_1554261232" MODIFIED="1516130200887" TEXT="Registrera en dom&#xe4;n och e-postadress ">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
 <node CREATED="1505233806745" ID="ID_382283949" MODIFIED="1505233809652" TEXT="one.com"/>
-<node CREATED="1505233810184" ID="ID_1345212169" MODIFIED="1505233813276" TEXT="go daddy"/>
 </node>
-<node CREATED="1505233871658" ID="ID_308489888" MODIFIED="1505234370897" POSITION="right" TEXT="Ans&#xf6;k om att bli godk&#xe4;nd f&#xf6;r F-skatt">
+<node CREATED="1505233871658" ID="ID_308489888" MODIFIED="1516130208077" TEXT="Ans&#xf6;k om att bli godk&#xe4;nd f&#xf6;r F-skatt">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
 </node>
-<node CREATED="1505233919650" ID="ID_1920650222" MODIFIED="1505234372655" POSITION="right" TEXT="Momsregistrera">
+<node CREATED="1505233919650" ID="ID_1920650222" MODIFIED="1516130214649" TEXT="Momsregistrera">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
 </node>
+<node CREATED="1505234040602" ID="ID_25997388" MODIFIED="1516130232620" TEXT="Uppr&#xe4;tta bokf&#xf6;ring">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1505234084461" ID="ID_1676175208" MODIFIED="1505234088079">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(0, 0, 0)" face="Liberation Sans, Arial, Verdana, sans-serif" size="16px">Alla ekonomiska transaktioner i f&#246;retaget ska bokf&#246;ras. Med transaktion menas n&#228;r du till exempel k&#246;per eller s&#228;ljer n&#229;got eller tar ett l&#229;n, det vill s&#228;ga n&#229;gon form av aff&#228;rsh&#228;ndelse.&#160;<br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px" /><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px" />N&#228;r r&#228;kenskaps&#229;ret &#228;r slut ska du uppr&#228;tta en &#229;rsredovisning. En &#229;rsredovisning best&#229;r av en f&#246;rvaltningsber&#228;ttelse, en resultatr&#228;kning, en balansr&#228;kning och till&#228;ggsupplysningar. &#197;rsredovisningen ska skickas till Bolagsverket.</font>
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1505234102267" ID="ID_151336566" MODIFIED="1516130494845" TEXT="V&#xe4;lj Revisor">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1505234172675" ID="ID_1889926454" MODIFIED="1505234187770" TEXT="Sm&#xe5; aktiebolag beh&#xf6;ver inte ha revisor"/>
+</node>
+</node>
+<node CREATED="1516131514667" ID="ID_1579360565" MODIFIED="1516131517101" POSITION="right" TEXT="Adwise">
+<node CREATED="1516131518325" ID="ID_1119617474" MODIFIED="1516131568820" TEXT="Fakturering sker m&#xe5;natligen"/>
+</node>
+<node CREATED="1516132755690" ID="ID_1958098110" MODIFIED="1516132759986" POSITION="right" TEXT="skatteverket">
+<node CREATED="1516132761700" ID="ID_1026222741" LINK="https://www.skatteverket.se/foretagochorganisationer/arbetsgivare/arbetsgivarregistrering/dittansvarsomarbetsgivare.4.361dc8c15312eff6fd16ec2.html" MODIFIED="1516132775497" TEXT="Ditt ansvar som arbetsgivare">
+<node CREATED="1516132868562" ID="ID_1610021066" MODIFIED="1516132869829" TEXT="skyldighet">
+<node CREATED="1516132883406" MODIFIED="1516132883406" TEXT="g&#xf6;ra skatteavdrag n&#xe4;r du betalar ut l&#xf6;n och annan ers&#xe4;ttning f&#xf6;r arbete"/>
+<node CREATED="1516132883406" MODIFIED="1516132883406" TEXT="betala arbetsgivaravgifter"/>
+<node CREATED="1516132883406" MODIFIED="1516132883406" TEXT="l&#xe4;mna arbetsgivardeklarationer (redovisa skatt och avgifter)"/>
+<node CREATED="1516132883406" MODIFIED="1516132883406" TEXT="l&#xe4;mna kontrolluppgifter."/>
+</node>
+<node CREATED="1516132927498" ID="ID_1123138269" MODIFIED="1516132932526" TEXT="Beh&#xf6;ver g&#xf6;ra">
+<node CREATED="1516132933471" ID="ID_1157960236" MODIFIED="1516132939013" TEXT="Vid anst&#xe4;llning">
+<node CREATED="1516132954490" ID="ID_703319590" MODIFIED="1516132954490" TEXT="Uppr&#xe4;tta ett anst&#xe4;llningsavtal."/>
+<node CREATED="1516132972214" ID="ID_1318272156" MODIFIED="1516132972871" TEXT="Ta reda p&#xe5; vilken skattetabell "/>
+<node CREATED="1516132986722" ID="ID_538328548" MODIFIED="1516132988065" TEXT="Registrera dig som arbetsgivare."/>
+</node>
+<node CREATED="1516132997703" ID="ID_772432682" MODIFIED="1516132999605" TEXT="L&#xf6;pande">
+<node CREATED="1516133017120" ID="ID_485340368" MODIFIED="1516133017120" TEXT="Betala ut l&#xf6;n till din anst&#xe4;llde">
+<node CREATED="1516133026396" ID="ID_894632511" MODIFIED="1516133066954">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(18, 18, 18)" face="Open Sans Regular, arial, sans-serif">Samtidigt ska du g&#246;ra ett <b>skatteavdrag</b>&#160;p&#229; l&#246;nen </font>
+    </p>
+    <p>
+      <font color="rgb(18, 18, 18)" face="Open Sans Regular, arial, sans-serif">enligt den skattetabell som du f&#229;tt fr&#229;n din </font>
+    </p>
+    <p>
+      <font color="rgb(18, 18, 18)" face="Open Sans Regular, arial, sans-serif">anst&#228;llde och r&#228;kna ut <b>arbetsgivaravgifter</b>.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1516133079336" ID="ID_1117753429" MODIFIED="1516133079977" TEXT="L&#xe4;mna en l&#xf6;nespecifikation"/>
+<node CREATED="1516133112098" ID="ID_1562275289" MODIFIED="1516133137368">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Arbetsgivaravgifter och skatteavdrag ska redovisas i en <b>arbetsgivardeklaration</b>.
+    </p>
+  </body>
+</html>
+</richcontent>
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1516133298251" ID="ID_1609774696" MODIFIED="1516133307935">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font color="rgb(18, 18, 18)" face="Open Sans Regular, arial, sans-serif" size="14px">Deklarationen f&#246;r en m&#229;nad l&#228;mnar du vanligtvis den 12:e i den <b>efterf&#246;ljande</b>&#160; m&#229;naden. Du l&#228;mnar allts&#229; in deklarationen f&#246;r m&#229;naden maj den 12:e juni.</font>
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1516133192946" ID="ID_1824992085" MODIFIED="1516133194292" TEXT="Betala till Skatteverket."/>
+</node>
+<node CREATED="1516133456969" ID="ID_1233411190" MODIFIED="1516133457610" TEXT="En g&#xe5;ng per &#xe5;r">
+<node CREATED="1516133468662" ID="ID_962943297" MODIFIED="1516133468662" TEXT="L&#xe4;mna kontrolluppgifter till Skatteverket."/>
+<node CREATED="1516133478287" ID="ID_1547546051" MODIFIED="1516133480039" TEXT="L&#xe4;mna kontrolluppgifter till din anst&#xe4;llde. "/>
+</node>
+</node>
+</node>
+<node CREATED="1516130881390" ID="ID_1136894411" MODIFIED="1516134725675" TEXT="prelimin&#xe4;r inkomstdeklaration."/>
+</node>
+<node CREATED="1516130575950" ID="ID_669989634" MODIFIED="1516130578191" POSITION="right" TEXT="2018">
+<node CREATED="1516130679007" ID="ID_1812095276" MODIFIED="1516130681515" TEXT="Jan"/>
+<node CREATED="1516130681922" ID="ID_443572061" MODIFIED="1516130683677" TEXT="Feb">
+<node COLOR="#990000" CREATED="1516131591499" ID="ID_858943035" MODIFIED="1516131703394" TEXT="1 Feb:Fakturera Adwise"/>
+<node CREATED="1516131716367" ID="ID_1049705336" MODIFIED="1516131743431" TEXT="9 Feb">
+<node CREATED="1516130655829" ID="ID_92660610" MODIFIED="1516131332143">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      l&#228;mna <b>momsdeklaration</b>&#160;senast 2018-02-12
+    </p>
+    <p>
+      (November - December 2017)
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1516132469029" ID="ID_671898460" MODIFIED="1516132469029" TEXT="12 feb        Du ska ha betalat in Moms"/>
+<node CREATED="1516132469029" ID="ID_1620540321" MODIFIED="1516132469029" TEXT="12 feb        Du ska ha betalat in Debiterad prelimin&#xe4;rskatt"/>
+<node CREATED="1516132469045" ID="ID_1442669349" MODIFIED="1516132469045" TEXT="12 feb        Du ska ha skickat in Momsdeklaration"/>
+<node CREATED="1516132842613" ID="ID_1662247570" MODIFIED="1516132858208" TEXT="19 Feb Registrera dig som arbetsgivare">
+<node CREATED="1516132954490" ID="ID_708183561" MODIFIED="1516132954490" TEXT="Uppr&#xe4;tta ett anst&#xe4;llningsavtal."/>
+<node CREATED="1516132972214" ID="ID_973743359" MODIFIED="1516132972871" TEXT="Ta reda p&#xe5; vilken skattetabell "/>
+<node CREATED="1516132986722" ID="ID_1286179440" MODIFIED="1516132988065" TEXT="Registrera dig som arbetsgivare."/>
+</node>
+</node>
+<node CREATED="1516130684141" ID="ID_1864719857" MODIFIED="1516130685895" TEXT="Mars">
+<node COLOR="#990000" CREATED="1516131591499" ID="ID_1549777905" MODIFIED="1516131764949" TEXT="1 Mars:Fakturera Adwise"/>
+<node CREATED="1516133242903" ID="ID_1321271453" MODIFIED="1516133256541" TEXT="27 Mars Utbetalning L&#xf6;n"/>
+</node>
+<node CREATED="1516130686487" ID="ID_1733297689" MODIFIED="1516130692575" TEXT="April">
+<node COLOR="#990000" CREATED="1516131591499" ID="ID_1229374857" MODIFIED="1516131779131" TEXT="1 April:Fakturera Adwise"/>
+<node CREATED="1516133340625" ID="ID_1195644690" MODIFIED="1516133375073" TEXT="12 April arbetsgivare deklaration f&#xf6;r Mars"/>
+<node CREATED="1516133340625" ID="ID_1806655575" MODIFIED="1516133409881" TEXT="12 April Betala till Skatteverket."/>
+</node>
+</node>
+<node CREATED="1516130647078" ID="ID_95634728" MODIFIED="1516130647078" POSITION="right" TEXT=""/>
 <node CREATED="1505233939300" ID="ID_320943283" MODIFIED="1505234374665" POSITION="right" TEXT="Registrera dig som arbetsgivare">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
 <node CREATED="1505233948835" ID="ID_1939118248" MODIFIED="1505233953879">
@@ -163,25 +307,6 @@
   </body>
 </html></richcontent>
 </node>
-</node>
-<node CREATED="1505234040602" ID="ID_25997388" MODIFIED="1505234377708" POSITION="right" TEXT="Uppr&#xe4;tta bokf&#xf6;ring">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505234084461" ID="ID_1676175208" MODIFIED="1505234088079">
-<richcontent TYPE="NODE"><html>
-  <head>
-    
-  </head>
-  <body>
-    <p>
-      <font color="rgb(0, 0, 0)" face="Liberation Sans, Arial, Verdana, sans-serif" size="16px">Alla ekonomiska transaktioner i f&#246;retaget ska bokf&#246;ras. Med transaktion menas n&#228;r du till exempel k&#246;per eller s&#228;ljer n&#229;got eller tar ett l&#229;n, det vill s&#228;ga n&#229;gon form av aff&#228;rsh&#228;ndelse.&#160;<br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px" /><br align="start" style="letter-spacing: normal; text-indent: 0px; text-transform: none; white-space: normal; word-spacing: 0px" />N&#228;r r&#228;kenskaps&#229;ret &#228;r slut ska du uppr&#228;tta en &#229;rsredovisning. En &#229;rsredovisning best&#229;r av en f&#246;rvaltningsber&#228;ttelse, en resultatr&#228;kning, en balansr&#228;kning och till&#228;ggsupplysningar. &#197;rsredovisningen ska skickas till Bolagsverket.</font>
-    </p>
-  </body>
-</html></richcontent>
-</node>
-</node>
-<node CREATED="1505234102267" ID="ID_151336566" MODIFIED="1505234379876" POSITION="right" TEXT="V&#xe4;lj Revisor">
-<font BOLD="true" NAME="SansSerif" SIZE="12"/>
-<node CREATED="1505234172675" ID="ID_1889926454" MODIFIED="1505234187770" TEXT="Sm&#xe5; aktiebolag beh&#xf6;ver inte ha revisor"/>
 </node>
 <node CREATED="1505234256166" ID="ID_1443696336" MODIFIED="1505234381636" POSITION="right" TEXT="Elektronisk skattedeklaration">
 <font BOLD="true" NAME="SansSerif" SIZE="12"/>
